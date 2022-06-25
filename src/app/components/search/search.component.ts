d4c4c6cb88689d9c060b4eeb201004190f58ca82
motheblackcat <morad.elhamdouni@gmail.com>
2022-06-25 15:32:42 +0200

    Add Leafet

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,10 @@
+import { Observable } from 'rxjs';
+import { IFlat } from 'src/app/interfaces/flat.interface';
+import { FlatService } from 'src/app/services/flat.service';
+
 import { Component, OnInit } from '@angular/core';
+
+declare let L: any
 
 @Component({
   selector: 'app-search',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  flats: Observable<IFlat[]> = new Observable<IFlat[]>;
 
-  constructor() { }
+  constructor(private flatService: FlatService) { }
 
   ngOnInit(): void {
+    this.flats = this.flatService.getFlats();
+
+    this.flats.subscribe(flats => {
+      this.initMap(flats);
+    });
+  }
+
+  initMap(flats: IFlat[]) {
+    console.log(flats);
+
+    const popup = `<p>${flats[0].adress}</p><p>${flats[0].description}</p>`;
+
+    const map = L.map('map').setView([46.000, 2.00], 6);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    const marker = L.marker([46.000, 2.00]).addTo(map);
+    marker.bindPopup(popup);
+
+    // const circle = L.circle([46.000, 2.00], {
+    //   color: 'blue',
+    //   fillColor: 'blue',
+    //   fillOpacity: 0.5,
+    //   radius: 100000
+    // }).addTo(map);
+
+    // map.on('click', (e: any) => console.log('clicked on', e));
   }
 
 }
